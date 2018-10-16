c2010be2900c79eb242c2d158f7070df4b92a359
20175227 <20175227@20175227.campus.tue.nl>
2018-10-16 11:57:33 +0200

    Robot joint size variables defined, robot position vectors changed to be relative to joint sizes.

--- src/robotrace/Robot.java
+++ src/robotrace/Robot.java
@@ -26,7 +26,7 @@ class Robot {
     private double legSize      = 0.4;
     private double armSize      = 0.3;
     private double headSize     = 0.4;
-   
+
     
     private Vector bodyPos = new Vector(0, 0, 0);
     private Vector headPos = new Vector(0, 0, (bodyHeight / 2) + (headSize / 2));
@@ -52,6 +52,33 @@ class Robot {
      */
     public void draw(GL2 gl, GLU glu, GLUT glut, float tAnim) {
         gl.glPushMatrix();
+            this.drawBody();
+        gl.glPopMatrix();
+        
+        gl.glPushMatrix();
+            gl.glTranslated(headPos.x, headPos.y, headPos.z);
+            this.drawHead();
+        gl.glPopMatrix();
+        
+        gl.glPushMatrix();
+            gl.glTranslated(leftArmPos.x, leftArmPos.y, leftArmPos.z);
+            this.drawArm();
+        gl.glPopMatrix();
+        
+        gl.glPushMatrix();
+            gl.glTranslated(rightArmPos.x, rightArmPos.y, rightArmPos.z);
+            this.drawArm();
+        gl.glPopMatrix();
+        
+        gl.glPushMatrix();
+            gl.glTranslated(leftLegPos.x, leftLegPos.y, leftLegPos.z);
+            this.drawLeg();
+        gl.glPopMatrix();
+        
+        gl.glPushMatrix();
+            gl.glTranslated(rightLegPos.x, rightLegPos.y, rightLegPos.z);
+            this.drawLeg();
+        gl.glPopMatrix();
     }
     
     private void drawBody(){
